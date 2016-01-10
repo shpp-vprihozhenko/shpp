f60151b5ef99eac0c91117b13ead0544a6795a53
shpp-vprihozhenko <vprihozhenko@gmail.com>
2016-01-10 11:32:58 +0200

    chat via AJAX.

--- day5-nodejs-basics/jq_aj_server.js
+++ day5-nodejs-basics/jq_aj_server.js
@@ -3,20 +3,83 @@
  */
 var express = require('express');
 var callbacks = [];
+var arLastGotID=[];
 var activeUsers=[];
-var activeUsersID=[];
 var totalUsers=0;
+var onlineRequest="who is online?";
+var personalMsgRequest="to:";
+
+function findLostUsers(){
+    var res=[];
+    for(var i=0;i<activeUsers.length;i++){
+        if(arLastGotID.indexOf(activeUsers[i].id)==-1){
+            if((activeUsers[i].userNickName!=undefined)&&(activeUsers[i].userNickName!=""))
+                res.push(activeUsers[i].userNickName);
+        }
+    }
+    return res;
+}
+
+function delLostUsersFromActive() {
+    for(var i=0;i<activeUsers.length;i++){
+        if(arLastGotID.indexOf(activeUsers[i].id)==-1){
+            activeUsers.splice(i,1);
+            i--;
+        }
+    }
+    totalUsers=activeUsers.length;
+}
+
+function findActiveUserID(toNick) {
+    for(var i=0;i<activeUsers.length;i++){
+        if(activeUsers[i].userNickName==toNick){
+            return activeUsers[i].id;
+        }
+    }
+    return "";
+}
+
+function sendPersonalMsg(message) {
+    message.text=message.text.substr(3).trim();
+    var toNick=message.text.substr(0,message.text.indexOf(" ")).trim();
+    var usrID=findActiveUserID(toNick);
+    if(usrID=="") return;
+    message.text="(personal msg) "+message.text;
+
+    var resp = {messages: [message]};
+
+    for (var i=0;i<callbacks.length;i++) {
+        if(arLastGotID[i]==usrID){
+            var fn=callbacks[i];
+            fn(resp);
+            callbacks.splice(i,1);
+            arLastGotID.splice(i,1);
+            break;
+        }
+    }
+}
 
 function sendMessageToListeners(message){
     var resp = {messages: [message]};
-    if(totalUsers>callbacks.length) {
-        totalUsers=callbacks.length;
 
+    if(callbacks.length<activeUsers.length){
+        var lostUsers=findLostUsers();
+        if(lostUsers.length>0){
+            var msg={};
+            msg.nickname=".server.";
+            msg.text="Disconnected from chat: "+lostUsers;
+            resp.messages.push(msg);
+        }
+        delLostUsersFromActive();
     }
-    while (callbacks.length > 0) {
-        var fn=callbacks.shift();
+
+    for (var i=0;i<callbacks.length;i++) {
+        var fn=callbacks[i];
         fn(resp);
     }
+
+    callbacks.splice(0,callbacks.length);
+    arLastGotID.splice(0,arLastGotID.length);
 }
 
 var express = require('express');
@@ -31,15 +94,30 @@ app.get('/', function(req, res){
 });
 
 app.post('/send', function(req, res) {
+
+    function formAnswer(message) {
+        var s="";
+        for(var i=0;i<activeUsers.length;i++){
+            s+= activeUsers[i].userNickName;
+            if(i<activeUsers.length-1) s+=", ";
+        }
+        message.nickname=".server."; message.text="Now online: "+s;
+    }
+
     var message = {
         nickname: req.param('nickname', 'Anonymous'),
-        text: req.param('text', '')
+        text: req.param('text', ''),
+        userID: req.param('userID', '')
     };
 
-    if(activeUsers.indexOf(message.nickname)==-1)
-        activeUsers.push(message.nickname);
-
-    sendMessageToListeners(message);
+    if(message.text.toLowerCase().indexOf(onlineRequest)>-1){
+        formAnswer(message)
+    }
+    if(message.text.toLowerCase().substr(0,3)==personalMsgRequest){
+        sendPersonalMsg(message);
+    } else {
+        sendMessageToListeners(message);
+    }
 
     res.json({status: 'ok'});
 });
@@ -55,10 +133,30 @@ app.get('/newUser', function(req, res){
 app.get('/recv', function(req, res){
     var userID=req.query.userID;
     var userNickName=req.query.userNickName;
+
+    function findUserIndexByID(activeUsers, userID) {
+        var res=-1;
+        for(var i=0; i<activeUsers.length; i++){
+            if(activeUsers[i].id==userID) return i;
+        }
+        return res;
+    }
+
+    var usrIdx=findUserIndexByID(activeUsers,userID);
+    if(usrIdx==-1){
+        var user={};
+        user.id=userID;
+        user.userNickName="";
+        activeUsers.push(user);
+    } else {
+        activeUsers[usrIdx].userNickName=userNickName;
+    }
+
+    arLastGotID.push(userID);
     callbacks.push(function(message){
         res.json(message);
     });
 });
 
-srv=app.listen(6611);//process.env.PORT
+srv=app.listen(6612);//process.env.PORT
 console.log(srv);//.ondisconnect()
