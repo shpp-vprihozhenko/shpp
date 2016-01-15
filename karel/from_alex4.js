78946ac29b7e8a0790fec59934240032fba6fa49
shpp-vprihozhenko <vprihozhenko@gmail.com>
2016-01-15 16:06:21 +0200

    karel. 15

--- karel/from_alex4.js
+++ karel/from_alex4.js
@@ -1,15 +1,15 @@
-/**
- * Created by Uzer on 15.01.2016.
- */
+///**
+// * Created by Uzer on 15.01.2016.
+// */
 //var a = ["1x x1x1x1x ",
 //    ">      1 1 ",
 //    "1x x2x1x1x "];
-
+//
 //var map = new Field(a);
 
 
-function Field(initMap) {
-    this.map = initMap;
+function Field(map) {
+    this.map = map;
 }
 
 Field.prototype.isAvailable = function (x, y) {
@@ -60,110 +60,43 @@ Karel.prototype.move = function () {
         if (map.isAvailable(this.x, this.y + 1)) {
             dY = 1;
         } else {
-            commands.push({
-                oldData: {
-                    x: this.x,
-                    y: this.y,
-                    dir: this.direction,
-                    beepers: map.checkBeeper(this.x, this.y)
-                },
-                newData: null
-            });
-            //listCommands.push(null);
+            this.x = -1;
+            this.y = -1;
             return;
         }
     } else if (this.direction == '1') {
         if (map.isAvailable(this.x + 1, this.y)) {
             dX = 1;
         } else {
-            commands.push({
-                oldData: {
-                    x: this.x,
-                    y: this.y,
-                    dir: this.direction,
-                    beepers: map.checkBeeper(this.x, this.y)
-                },
-                newData: null
-            });
-            //listCommands.push(null);
+            this.x = -1;
+            this.y = -1;
             return;
         }
     } else if (this.direction == '2') {
         if (map.isAvailable(this.x, this.y + 1)) {
             dY = -1;
         } else {
-            commands.push({
-                oldData: {
-                    x: this.x,
-                    y: this.y,
-                    dir: this.direction,
-                    beepers: map.checkBeeper(this.x, this.y)
-                },
-                newData: null
-            });
-            //listCommands.push(null);
+            this.x = -1;
+            this.y = -1;
             return;
         }
     } else if (this.direction == '3') {
         if (map.isAvailable(this.x - 1, this.y)) {
             dX = -1;
         } else {
-            commands.push({
-                oldData: {
-                    x: this.x,
-                    y: this.y,
-                    dir: this.direction,
-                    beepers: map.checkBeeper(this.x, this.y)
-                },
-                newData: null
-            });
-            //listCommands.push(null);
+            this.x = -1;
+            this.y = -1;
             return;
         }
     }
     var newX = this.x + dX;
     var newY = this.y + dY;
-    commands.push({
-        oldData: {
-            x: this.x,
-            y: this.y,
-            dir: this.direction,
-            beepers: map.checkBeeper(this.x, this.y)
-        },
-        newData: {x: newX, y: newY, dir: this.direction, beepers: map.checkBeeper(newX, newY)}
-    });
-
-    /*listCommands.push({
-     x1: this.x,
-     y1: this.y,
-     direction1: this.direction,
-     x2: newX,
-     y2: newY,
-     direction2: this.direction
-     });*/
     this.x = newX;
     this.y = newY;
 }
 
 Karel.prototype.turnLeft = function () {
     var newDirection = (this.direction + 1) % 4;
-    commands.push({
-        oldData: {
-            x: this.x,
-            y: this.y,
-            dir: this.direction,
-            beepers: map.checkBeeper(this.x, this.y)
-        },
-        newData: {x: this.x, y: this.y, dir: newDirection, beepers: map.checkBeeper(this.x, this.y)}
-    });
-    /*listCommands.push({
-     x1: this.x,
-     y1: this.y,
-     direction1: this.direction,
-     x2: this.x,
-     y2: this.y,
-     direction2: newDirection
-     });*/
     this.direction = newDirection;
 }
 
@@ -332,55 +265,64 @@ Karel.prototype.noFacingEast = function () {
 
 //var karelPosition = map.getKarelPosition();
 //var karel = new Karel(karelPosition.x, karelPosition.y, karelPosition.direction);
+
 //var commands = [];
 
-function parseTask(lines,commands,karel) {
+function parseTask(lines, arMoves, karel) {
     for (var i = 0; i < lines.length; i++) {
-        if (lines[i] === 'move();') {
-            karel.move();
-        } else if (lines[i] === 'turnLeft();') {
-            karel.turnLeft();
-        } else if (lines[i] === 'frontIsClear();') {
-            karel.frontIsClear();
-            console.log("front", karel.frontIsClear());
-        } else if (lines[i] === 'frontIsBlocked();') {
-            karel.frontIsBlocked();
-            console.log("front", karel.frontIsBlocked());
-        } else if (lines[i] === 'rightIsClear();') {
-            karel.rightIsClear();
-            console.log("right", karel.rightIsClear());
-        } else if (lines[i] === 'rightIsBlocked();') {
-            karel.rightIsBlocked();
-        } else if (lines[i] === 'leftIsClear();') {
-            karel.leftIsClear();
-            console.log("left", karel.leftIsClear());
-        } else if (lines[i] === 'leftIsBlocked();') {
-            karel.leftIsBlocked();
-        } else if (lines[i] === 'beepersPresent();') {
-            karel.beepersPresent();
-        } else if (lines[i] === 'noBeepersPresent();') {
-            karel.noBeepersPresent();
-        } else if (lines[i] === 'facingNorth();') {
-            karel.facingNorth();
-        } else if (lines[i] === 'noFacingNorth();') {
-            karel.noFacingNorth();
-        } else if (lines[i] === 'facingEast();') {
-            karel.facingEast();
-        } else if (lines[i] === 'noFacingEast();') {
-            karel.noFacingEast();
-        } else if (lines[i] === 'facingSouth();') {
-            karel.facingSouth();
-        } else if (lines[i] === 'noFacingSouth();') {
-            karel.noFacingSouth();
-        } else if (lines[i] === 'facingWest();') {
-            karel.facingWest();
-        } else if (lines[i] === 'noFacingWest();') {
-            karel.noFacingWest();
-        } else if (lines[i] === 'putBeeper();') {
-            karel.putBeeper();
-        } else if (lines[i] === 'pickBeeper();') {
-            karel.pickBeeper();
-        }
+        var oldX = karel.x;
+        var oldY = karel.y;
+        var oldDir = karel.direction;
+        var oldBeepers = map.checkBeeper(oldX, oldY);
+        checkCommands(lines[i]);
+        var newX = karel.x;
+        var newY = karel.y;
+        var newDir = karel.direction;
+        var newBeepers = map.checkBeeper(newX, newY);
+        arMoves.push({oldData:{x:oldX, y:oldY, dir:oldDir, beepers: oldBeepers}, newData:{x:newX, y:newY, dir:newDir, beepers: newBeepers}})
     }
-    return commands;
+}
+
+function checkCommands(command) {
+    if (command.indexOf('move') > -1) {
+        karel.move();
+    } else if (command.indexOf('turnLeft') > -1) {
+        karel.turnLeft();
+    } else if (command.indexOf('frontIsClear') > -1) {
+        karel.frontIsClear();
+    } else if (command.indexOf('frontIsBlocked') > -1) {
+        karel.frontIsBlocked();
+    } else if (command.indexOf('rightIsClear') > -1) {
+        karel.rightIsClear();
+    } else if (command.indexOf('rightIsBlocked') > -1) {
+        karel.rightIsBlocked();
+    } else if (command.indexOf('leftIsClear') > -1) {
+        karel.leftIsClear();
+    } else if (command.indexOf('leftIsBlocked') > -1) {
+        karel.leftIsBlocked();
+    } else if (command.indexOf('beepersPresent') > -1) {
+        karel.beepersPresent();
+    } else if (command.indexOf('noBeepersPresent') > -1) {
+        karel.noBeepersPresent();
+    } else if (command.indexOf('facingNorth') > -1) {
+        karel.facingNorth();
+    } else if (command.indexOf('noFacingNorth') > -1) {
+        karel.noFacingNorth();
+    } else if (command.indexOf('facingEast') > -1) {
+        karel.facingEast();
+    } else if (command.indexOf('noFacingEast') > -1) {
+        karel.noFacingEast();
+    } else if (command.indexOf('facingSouth') > -1) {
+        karel.facingSouth();
+    } else if (command.indexOf('noFacingSouth') > -1) {
+        karel.noFacingSouth();
+    } else if (command.indexOf('facingWest') > -1) {
+        karel.facingWest();
+    } else if (command.indexOf('noFacingWest') > -1) {
+        karel.noFacingWest();
+    } else if (command.indexOf('putBeeper') > -1) {
+        karel.putBeeper();
+    } else if (command.indexOf('pickBeeper') > -1) {
+        karel.pickBeeper();
+    }
 }
